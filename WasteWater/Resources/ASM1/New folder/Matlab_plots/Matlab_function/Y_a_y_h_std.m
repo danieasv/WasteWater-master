ed0cdcd48101d7b5947f3f0629857a19877cf9e4
Daniel Svendsen <31532543+danieasv@users.noreply.github.com>
2018-04-24 12:07:52 +0200

    Added more plots in folder
    Added plot for Ya_Yh std and Ya_yh divide by 10 and more

---
+++ WasteWater/Resources/ASM1/New folder/Matlab_plots/Matlab_function/Y_a_y_h_std.m
@@ -0,0 +1,8 @@
+
+title('Y_a = 0.24 and Y_h = 0.67 [Standard]')
+plot(yayh_std(:,1),yayh_std(:,2),yayh_std(:,1),yayh_std(:,3),yayh_std(:,1),yayh_std(:,4),...
+yayh_std(:,1),yayh_std(:,5),yayh_std(:,1),yayh_std(:,6),yayh_std(:,1),yayh_std(:,7));
+xlabel("Time");
+ylabel('Values');
+legend('EffluentQualityEQ.Ya', 'WasteSludge.In.Salk','WasteSludge.In.Sno','WasteSludge.In.Snh',...
+    'WasteSludge.In.So','WasteSludge.In.Ss')
